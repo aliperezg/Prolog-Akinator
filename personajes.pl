8207348377f62a6afc5ff421885b8df05435e093
Diego Rivera <d.rivera@beehivesystems.mx>
2026-04-23 22:18:21 -0600

    Add personajes.pl with GENERATD TEST DATA

---
+++ personajes.pl
@@ -0,0 +1,32 @@
+% personajes.pl — AUTO-GENERADO por generate.py. NO EDITAR.
+
+atributo(mickey_mouse, especie, animal).
+atributo(mickey_mouse, es_villano, no).
+atributo(mickey_mouse, es_protagonista, si).
+atributo(mickey_mouse, color_pelo, negro).
+atributo(mickey_mouse, pelo_largo, no).
+atributo(mickey_mouse, es_magico, no).
+atributo(mickey_mouse, color_vestimenta, rojo).
+atributo(mickey_mouse, vive_en, ciudad).
+atributo(mickey_mouse, genero, masculino).
+
+atributo(minnie_mouse, especie, animal).
+atributo(minnie_mouse, es_villano, no).
+atributo(minnie_mouse, es_protagonista, si).
+atributo(minnie_mouse, color_pelo, negro).
+atributo(minnie_mouse, pelo_largo, si).
+atributo(minnie_mouse, es_magico, no).
+atributo(minnie_mouse, color_vestimenta, rosa).
+atributo(minnie_mouse, vive_en, ciudad).
+atributo(minnie_mouse, genero, femenino).
+
+atributo(daisy_duck, especie, animal).
+atributo(daisy_duck, es_villano, no).
+atributo(daisy_duck, es_protagonista, si).
+atributo(daisy_duck, color_pelo, ninguno).
+atributo(daisy_duck, pelo_largo, si).
+atributo(daisy_duck, es_magico, no).
+atributo(daisy_duck, color_vestimenta, morado).
+atributo(daisy_duck, vive_en, ciudad).
+atributo(daisy_duck, genero, femenino).
+
