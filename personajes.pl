1c3787d2f60dd691803a16040b40e500d33d8899
Diego Rivera <d.rivera@beehivesystems.mx>
2026-04-23 23:13:28 -0600

    Regenerated personajes.pl

--- personajes.pl
+++ personajes.pl
@@ -4,7 +4,7 @@ atributo(mickey_mouse, especie, animal).
 atributo(mickey_mouse, es_villano, no).
 atributo(mickey_mouse, es_protagonista, si).
 atributo(mickey_mouse, color_pelo, negro).
-atributo(mickey_mouse, pelo_largo, no).
+atributo(mickey_mouse, pelo, corto).
 atributo(mickey_mouse, es_magico, no).
 atributo(mickey_mouse, color_vestimenta, rojo).
 atributo(mickey_mouse, vive_en, ciudad).
@@ -14,7 +14,7 @@ atributo(minnie_mouse, especie, animal).
 atributo(minnie_mouse, es_villano, no).
 atributo(minnie_mouse, es_protagonista, si).
 atributo(minnie_mouse, color_pelo, negro).
-atributo(minnie_mouse, pelo_largo, si).
+atributo(minnie_mouse, pelo, corto).
 atributo(minnie_mouse, es_magico, no).
 atributo(minnie_mouse, color_vestimenta, rosa).
 atributo(minnie_mouse, vive_en, ciudad).
@@ -24,7 +24,7 @@ atributo(daisy_duck, especie, animal).
 atributo(daisy_duck, es_villano, no).
 atributo(daisy_duck, es_protagonista, si).
 atributo(daisy_duck, color_pelo, ninguno).
-atributo(daisy_duck, pelo_largo, si).
+atributo(daisy_duck, pelo, sin_pelo).
 atributo(daisy_duck, es_magico, no).
 atributo(daisy_duck, color_vestimenta, morado).
 atributo(daisy_duck, vive_en, ciudad).
